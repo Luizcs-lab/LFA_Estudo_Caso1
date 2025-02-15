38a456f49a904bc5ed2234dd9acf6b306c462c57
cesar luiz da silva <cesarluizdasilva10@gmail.com>
2025-02-15 16:16:07 -0300

    Adicionando funcionalidade para leitura de arquivos Logs

--- Pipeline.py
+++ Pipeline.py
@@ -2,11 +2,32 @@
 # 1- Ler o arquivo de Log
 # 2- Usar Token automatizado com IA para classificação e identificar padrão
 # 3- Transformar/formatar os dados do Log
-# 4- Saída, salavar as informações relevantes em novo arquivo
+# 4- Saída, salvar as informações relevantes em novo arquivo
 
 #importando bibliotecas para o desenvolvimento do projeto de estudo de caso 1
 #Bibliotecas que serão usadas:
-#import re para Token, import Pandas para pipeline e (automação de IA) ou import Dask, import pyoutogui para automação
+#import re para Token, import Pandas para pipeline e pyautogui (automação de IA) ou import Dask, import scikit-learn para machine learning para classificação
 import re
-import pandas as pd # type: ignore
-import pyautogui  # type: ignore
+import pandas as pd
+import pyautogui
+import scikit-learn
+
+#Desenvolvimento das etapas do pipeline:
+# 1-Etapa-> Ler arquivo de Log usando da biblioteca do pandas e suas funcionalidades 
+    def Ler_Arquivo_Log(arquivolog):
+       with open(arquivolog) as a:
+        for linha in a:
+            linha.strip()
+
+# 2-Etapa-> Usar Tokenização de texto para dividir o conteúdo e usar em conjunto o sciki-learn (machine learning) para classificar os dados
+    def Extracao():
+    
+
+# 3-Etapa-> Transformar ou formatar os dados relevantes de forma automatizada
+    def Tratamento():
+
+# 4-Etapa-> A saída dos dados para um novo arquivo(dados que foram tratados)
+    def Conversao():
+
+# Execução das etapas:
+
