91b04236324341f2c7e12880e9828fbc6aef486d
cesar luiz da silva <cesarluizdasilva10@gmail.com>
2025-02-25 17:04:55 -0300

    condicional das extensoes do arquivo selecionado

--- Pipeline.py
+++ Pipeline.py
@@ -15,18 +15,26 @@ import customtkinter as ctk
 from tkinter import Tk
 from tkinter.filedialog import askopenfile
 from tkinter.filedialog import FileDialog 
-#criando a interface do programa
+#criando a interface do programa e sua cor de fundo
 ctk.set_appearance_mode('ligth')
-#função para abrir o explorador de arquivos para o usuário
+#função para abrir o explorador de arquivos para o usuário selecionar
 def prosseguir():
     # permite a seleção do arquivo dentro do explorador e retorna para o usuário
         Tk().withdraw()
         nomearquivo = askopenfile()
         print(nomearquivo)
+        extensoes = (".log",".txt",".csv")
+        if nomearquivo.endswith == extensoes:
+            with open(nomearquivo, "r") as arquivo:
+                print(arquivo.read())
+        
+                
 #--------------------------------------------------------------------      
-    # definição da janela
+# definição da janela
 app = ctk.CTk()
+#Titulo da janela
 app.title("Pipeline Python")
+#Dimensionamento da janela altura e largura
 app.geometry("550x500")
 # texto de boas vindas
 Label = ctk.CTkLabel(
@@ -36,13 +44,13 @@ Label.pack(pady=15)
 texto = ctk.CTkTextbox(app, height=12 )
 texto.pack(pady=10)
 
-texto.grid(column=0, rows=0, sticky='nsew')
-# Botão para prosseguir com a aplicação
+
+# Botão para prosseguir com a aplicação e mostrar a janela do explorador de arquivos chamando a função prosseguir
 botao = ctk.CTkButton(app, text="prosseguir", command=prosseguir)
 botao.pack(pady=10)
 
 app.mainloop()
-#--------------------------------------------------------------------------------------
+#----------------------------------fim-da-janela----------------------------------------------------
 
 # Desenvolvimento das etapas do pipeline:
 # 1-Etapa-> Ler arquivo de Log usando da biblioteca do pandas e suas funcionalidades
