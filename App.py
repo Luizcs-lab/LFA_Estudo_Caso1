2788b995574636b6e58ef8a07fdff42c74281549
cesar luiz da silva <cesarluizdasilva10@gmail.com>
2025-05-20 18:55:20 -0300

    corrigido funcionalidades

--- App.py
+++ App.py
@@ -1,5 +1,7 @@
 # app.py
+from ui.splash_screen import mostrar_splash
 from ui.interface_principal import criar_interface
 
 if __name__ == '__main__':
+   mostrar_splash(duracao=5)
    criar_interface()
